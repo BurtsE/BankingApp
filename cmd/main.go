f95185c641eea8119fe509991cb844f92fca55ae
burce <burcev-egor@mail.ru>
2025-05-15 13:59:46 +0500

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
-package cmd
+package main
+
+import (
+	"BankingApp/internal/app"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		cancel()
+	}()
+	app := app.NewApp(ctx)
+	app.Run()
 }
